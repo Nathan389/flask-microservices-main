f9626a04eca7f874ba6a1c7f865f84bea6279598
Nathan Muschinske <nathan389@gmail.com>
2017-08-22 11:36:30 -0500

    Added messages and reconciled code with tutorial source code.

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 fails=''
 
 inspect() {
-    if [ $1 -ne 0 ]; then
-        fails="${fails} $2"
-    fi
+  if [ $1 -ne 0 ]; then
+    fails="${fails} $2"
+  fi
 }
 
 docker-compose run users-service python manage.py test
@@ -15,10 +15,10 @@ testcafe chrome e2e
 inspect $? e2e
 
 if [ -n "${fails}" ];
-    then
-        echo "Tests fails: ${fails}"
-        exit 1
-    else
-        echo "Tests passed!"
-        echo 0
+  then
+    echo "Tests failed: ${fails}"
+    exit 1
+  else
+    echo "Tests passed!"
+    exit 0
 fi
